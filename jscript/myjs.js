3fb85723485af2f34f02c5ca832fbb20bf5ede6b
Umberto <umbertopellegrino96@gmail.com>
2020-06-12 15:10:25 +0200

    Prima stesura ex

---
+++ jscript/myjs.js
@@ -0,0 +1,11 @@
+// Chiedi all’utente il suo nome,poi chiedi il suo cognome,
+//poi chiedi il suo colore preferito.
+
+var name= prompt("Per creare una password perfetta inserisci qui il tuo nome :");
+var cognome= prompt("Ora il tuo cognome :");
+var colorePref= prompt("E in fine il tuo colore preferito:");
+
+var passfinale= name + cognome + colorePref + 19;
+//Infine scrivi sulla pagina nomecognomecolorepreferito19
+
+document.getElementById("passw").innerHTML= passfinale;

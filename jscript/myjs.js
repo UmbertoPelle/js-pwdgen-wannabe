b40c625d6d56b2464707d3833249a71b978eb7d6
Umberto <umbertopellegrino96@gmail.com>
2020-06-12 15:14:45 +0200

    Correzione ex

--- jscript/myjs.js
+++ jscript/myjs.js
@@ -6,6 +6,8 @@ var cognome= prompt("Ora il tuo cognome :");
 var colorePref= prompt("E in fine il tuo colore preferito:");
 
 var passfinale= name + cognome + colorePref + 19;
+
 //Infine scrivi sulla pagina nomecognomecolorepreferito19
 
+document.getElementById("titolo").innerHTML="Hai generato una PASSWORD INFALLIBILE: "
 document.getElementById("passw").innerHTML= passfinale;
